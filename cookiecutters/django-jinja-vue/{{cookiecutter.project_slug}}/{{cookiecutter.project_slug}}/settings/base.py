4fbcd9a50d8d3239d2fdc7bc7a38196633aeca2b
prokaktus <proktusfahitasiv@gmail.com>
2018-11-03 21:11:41 +0300

    Fix missing dirname

--- cookiecutters/django-jinja-vue/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
+++ cookiecutters/django-jinja-vue/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -18,7 +18,7 @@ from ._utils import getenv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
